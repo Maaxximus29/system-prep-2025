8f3ee4270ccf0f057c07a9a38ab62686ca1e263d
pranav <pranavraipur03@gmail.com>
2025-07-02 02:21:01 +0000

    volume of a sphere

---
+++ volume.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define pi 3.14
+
+int main(void)
+{
+    //radius
+    int radius = 10;
+    float volume = (4.0f/3.0f) * pi * radius * radius * radius;
+    printf("The volume of sphere is %.2f\n", volume);
+
+return 0;
+
+}
